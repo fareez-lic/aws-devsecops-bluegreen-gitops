c768c9b127086df5aa85ed960622ab984f347c3d
fareez-lic <fareez.lic@gmail.com>
2026-09-16 02:50:55 -0400

    infra: add NAT gateway for private subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,3 +117,55 @@ resource "aws_ecr_repository" "app" {
 
   tags = local.common_tags
 }
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-nat-eip"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+
+  depends_on = [
+    aws_internet_gateway.main
+  ]
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-nat-gateway"
+    }
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-private-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
