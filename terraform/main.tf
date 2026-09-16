4672c17bd137487315a2bf5b839c33c6bb938334
fareez-lic <fareez.lic@gmail.com>
2026-09-16 01:55:12 -0400

    infra: add ECR repository

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,3 +107,13 @@ resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.project_name}-app"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = local.common_tags
+}
